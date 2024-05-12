2ea526a595beca2d51d9eb88321f8b719c80921f
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-05-12 16:59:42 -0400

    Styled Board Component

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Board.css";
+import Tile from "../Tile/Tile";
 
 export default function Board() {
-  return <div>Board Component</div>;
+  return <div id="board" className="center"></div>;
 }
