3443c4130f9c2fbfc438b126c2fe5216352e0036
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-05-12 17:55:59 -0400

    Able to render board data

--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Tile.css";
 
-export default function Tile() {
-  return <div id="tile"></div>;
+export default function Tile({ tile, index }) {
+  return (
+    <div id="tile" key={"tile_" + index}>
+      {tile}
+    </div>
+  );
 }
