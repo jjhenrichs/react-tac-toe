19082b13ba622bf0c8648cd5c0ae2ed5f5963a08
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-05-12 20:16:38 -0400

    Added turn and pattern variables

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Board from "../Board/Board";
 
 function App() {
   const board = ["X", "X", "", "", "", "", "", "", ""];
+
+  const [turn, setTurn] = useState(1);
+
+  const pattern = turn % 2 === 1 ? "X" : "O";
 
   return (
     <div className="App">
--- src/Tile/Tile.js
+++ src/Tile/Tile.js
@@ -3,7 +3,7 @@ import "./Tile.css";
 
 export default function Tile({ tile, index }) {
   return (
-    <div id="tile" key={"tile_" + index}>
+    <div id="tile" className="center" key={"tile_" + index}>
       {tile}
     </div>
   );
